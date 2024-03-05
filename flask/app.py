82fad1525f0aebd89db45c8f63ad3e696ec7d6f9
Nicholas Hale <100668433+nrhale@users.noreply.github.com>
2024-03-05 19:35:11 +0000

    Moved image locations and completed flask boilerplate

--- flask/app.py
+++ flask/app.py
@@ -2,12 +2,43 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/home")
 def home():
-    return render_template('index.html')
+    return render_template("home.html")
 
-def signin():
-    return "<h1>Sign In</h1>"
 
-if __name__ == '__main__':
+@app.route("/statistics")
+def statistics():
+    return render_template("statistics.html")
+
+
+@app.route("/modules")
+def modules():
+    return render_template("modules.html")
+
+
+@app.route("/learn/<module>/<sign>")
+def learn(module, sign):
+    return render_template("learn.html", module=module, sign=sign)
+
+
+@app.route("/assessment/<module>/<assessmentType>")
+def assessment(module, assessmentType):
+    return render_template(
+        "assessment.html", module=module, assessmentType=assessmentType
+    )
+
+
+@app.route("/sandbox/<module>")
+def sandbox(module):
+    return render_template("sandbox.html", module=module)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
