19fe105762b765d12e48c87bd8e738292c20cb55
price-3 <micprice437@gmail.com>
2024-03-10 15:44:11 -0230

    Changed module names

--- price_testing/common.py
+++ price_testing/common.py
@@ -13,8 +13,8 @@ STATE_SANDBOX = 7
 
 # Some Default information. Will change later
 #s1 = Sign("A", 0, 0, "alphabet1")
-s2 = Sign("B", 0, 0, "alphabet1")
-s3 = Sign("C", 0, 0, "alphabet1")
+s2 = Sign("B", 0, 0, "The Alphabet I")
+s3 = Sign("C", 0, 0, "The Alphabet I")
 
 #s4 = Sign("D", 0, 0, "alphabet1")
 #s5 = Sign("E", 0, 0, "alphabet1")
@@ -26,10 +26,10 @@ s_list2 = []
 s_list3 = []
 s_list4 = []
 
-MOD1 = Module("alphabet1", "valid_alphabet_a_to_f", s_list1, 0)
-MOD2 = Module("alphabet2", "valid_alphabet_g_to_m", s_list2, 0)
-MOD3 = Module("alphabet3", "valid_alphabet_n_to_s", s_list3, 0)
-MOD4 = Module("alphabet4", "valid_alphabet_t_to_y", s_list4, 0)
+MOD1 = Module("The Alphabet I", "valid_alphabet_a_to_f", s_list1, 0)
+MOD2 = Module("The Alphabet II", "valid_alphabet_g_to_m", s_list2, 0)
+MOD3 = Module("The Alphabet III", "valid_alphabet_n_to_s", s_list3, 0)
+MOD4 = Module("The Alphabet IV", "valid_alphabet_t_to_y", s_list4, 0)
 
 # Extract descriptions from descriptions.txt
 descriptions = {}
@@ -52,33 +52,33 @@ with open('video_urls.txt', 'r') as file:
             urls[item.strip()] = url.strip()
             
 # Sign Info (always stays the same)
-siA = SignInfo("A", descriptions.get('A'), urls.get('A'), "../asl_images/letter_a.svg", "alphabet1")
-siB = SignInfo("B", descriptions.get('B'), urls.get('B'), "../asl_images/letter_b.svg", "alphabet1")
-siC = SignInfo("C", descriptions.get('C'), urls.get('C'), "../asl_images/letter_c.svg", "alphabet1")
-siD = SignInfo("D", descriptions.get('D'), urls.get('D'), "../asl_images/letter_d.svg", "alphabet1")
-siE = SignInfo("E", descriptions.get('E'), urls.get('E'), "../asl_images/letter_e.svg", "alphabet1")
-siF = SignInfo("F", descriptions.get('F'), urls.get('F'), "../asl_images/letter_f.svg", "alphabet1")
+siA = SignInfo("A", descriptions.get('A'), urls.get('A'), "../asl_images/letter_a.svg", "The Alphabet I")
+siB = SignInfo("B", descriptions.get('B'), urls.get('B'), "../asl_images/letter_b.svg", "The Alphabet I")
+siC = SignInfo("C", descriptions.get('C'), urls.get('C'), "../asl_images/letter_c.svg", "The Alphabet I")
+siD = SignInfo("D", descriptions.get('D'), urls.get('D'), "../asl_images/letter_d.svg", "The Alphabet I")
+siE = SignInfo("E", descriptions.get('E'), urls.get('E'), "../asl_images/letter_e.svg", "The Alphabet I")
+siF = SignInfo("F", descriptions.get('F'), urls.get('F'), "../asl_images/letter_f.svg", "The Alphabet I")
 
-siG = SignInfo("G", descriptions.get('G'), urls.get('G'), "../asl_images/letter_g.svg", "alphabet2")
-siH = SignInfo("H", descriptions.get('H'), urls.get('H'), "../asl_images/letter_h.svg", "alphabet2")
-siI = SignInfo("I", descriptions.get('I'), urls.get('I'), "../asl_images/letter_i.svg", "alphabet2")
-siK = SignInfo("K", descriptions.get('K'), urls.get('K'), "../asl_images/letter_k.svg", "alphabet2")
-siL = SignInfo("L", descriptions.get('L'), urls.get('L'), "../asl_images/letter_l.svg", "alphabet2")
-siM = SignInfo("M", descriptions.get('M'), urls.get('M'), "../asl_images/letter_m.svg", "alphabet2")
+siG = SignInfo("G", descriptions.get('G'), urls.get('G'), "../asl_images/letter_g.svg", "The Alphabet II")
+siH = SignInfo("H", descriptions.get('H'), urls.get('H'), "../asl_images/letter_h.svg", "The Alphabet II")
+siI = SignInfo("I", descriptions.get('I'), urls.get('I'), "../asl_images/letter_i.svg", "The Alphabet II")
+siK = SignInfo("K", descriptions.get('K'), urls.get('K'), "../asl_images/letter_k.svg", "The Alphabet II")
+siL = SignInfo("L", descriptions.get('L'), urls.get('L'), "../asl_images/letter_l.svg", "The Alphabet II")
+siM = SignInfo("M", descriptions.get('M'), urls.get('M'), "../asl_images/letter_m.svg", "The Alphabet II")
 
-siN = SignInfo("N", descriptions.get('N'), urls.get('N'), "../asl_images/letter_n.svg", "alphabet3")
-siO = SignInfo("O", descriptions.get('O'), urls.get('O'), "../asl_images/letter_o.svg", "alphabet3")
-siP = SignInfo("P", descriptions.get('P'), urls.get('P'), "../asl_images/letter_p.svg", "alphabet3")
-siQ = SignInfo("Q", descriptions.get('Q'), urls.get('Q'), "../asl_images/letter_q.svg", "alphabet3")
-siR = SignInfo("R", descriptions.get('R'), urls.get('R'), "../asl_images/letter_r.svg", "alphabet3")
-siS = SignInfo("S", descriptions.get('S'), urls.get('S'), "../asl_images/letter_s.svg", "alphabet3")
+siN = SignInfo("N", descriptions.get('N'), urls.get('N'), "../asl_images/letter_n.svg", "The Alphabet III")
+siO = SignInfo("O", descriptions.get('O'), urls.get('O'), "../asl_images/letter_o.svg", "The Alphabet III")
+siP = SignInfo("P", descriptions.get('P'), urls.get('P'), "../asl_images/letter_p.svg", "The Alphabet III")
+siQ = SignInfo("Q", descriptions.get('Q'), urls.get('Q'), "../asl_images/letter_q.svg", "The Alphabet III")
+siR = SignInfo("R", descriptions.get('R'), urls.get('R'), "../asl_images/letter_r.svg", "The Alphabet III")
+siS = SignInfo("S", descriptions.get('S'), urls.get('S'), "../asl_images/letter_s.svg", "The Alphabet III")
 
-siT = SignInfo("T", descriptions.get('T'), urls.get('T'), "../asl_images/letter_t.svg", "alphabet4")
-siU = SignInfo("U", descriptions.get('U'), urls.get('U'), "../asl_images/letter_u.svg", "alphabet4")
-siV = SignInfo("V", descriptions.get('V'), urls.get('V'), "../asl_images/letter_v.svg", "alphabet4")
-siW = SignInfo("W", descriptions.get('W'), urls.get('W'), "../asl_images/letter_w.svg", "alphabet4")
-siX = SignInfo("X", descriptions.get('X'), urls.get('X'), "../asl_images/letter_x.svg", "alphabet4")
-siY = SignInfo("Y", descriptions.get('Y'), urls.get('Y'), "../asl_images/letter_y.svg", "alphabet4")
+siT = SignInfo("T", descriptions.get('T'), urls.get('T'), "../asl_images/letter_t.svg", "The Alphabet IV")
+siU = SignInfo("U", descriptions.get('U'), urls.get('U'), "../asl_images/letter_u.svg", "The Alphabet IV")
+siV = SignInfo("V", descriptions.get('V'), urls.get('V'), "../asl_images/letter_v.svg", "The Alphabet IV")
+siW = SignInfo("W", descriptions.get('W'), urls.get('W'), "../asl_images/letter_w.svg", "The Alphabet IV")
+siX = SignInfo("X", descriptions.get('X'), urls.get('X'), "../asl_images/letter_x.svg", "The Alphabet IV")
+siY = SignInfo("Y", descriptions.get('Y'), urls.get('Y'), "../asl_images/letter_y.svg", "The Alphabet IV")
 
 
 
