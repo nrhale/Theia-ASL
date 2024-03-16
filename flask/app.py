abedfe04d1514bb9e9f443e49576178bbd83937c
price-3 <micprice437@gmail.com>
2024-03-15 22:51:48 -0230

    Working basic assessment, score

--- flask/app.py
+++ flask/app.py
@@ -25,6 +25,10 @@ user_mod_data = []
 si = None
 chosen_mod = None
 chosen_sign = None
+remaining_list = []
+assess_done = False
+is_first_sign = None
+score = 0
 
 app = Flask(__name__)
 # camera = cv2.VideoCapture(0)
@@ -35,6 +39,7 @@ app = Flask(__name__)
 print("SANDBOX FLASK")
 
 
+"""
 def generate_output(module):
     # Your while loop logic here
     remaining_list = module.sign_name_list.copy()
@@ -67,17 +72,67 @@ def generate_output(module):
 
     # Yield the final score
     yield f"data: Score: {score}/{len(module.sign_name_list)}\n\n"
+"""
+
+def mar15_assessment(module, chosen_sign):
+    # Your while loop logic here
+    global score
+    global remaining_list
+    #remaining_list = module.sign_name_list.copy()
+    model_name = module.model
+    sign_name_list = create_si_name_list(SI_LIST, module.module_name)
+    #score = 0
+    print(f"chosen sign: {chosen_sign}")
+    #chosen_sign = None
+
+    result = "NO RESULT FOLKS"
+
+#if len(remaining_list) > 0:
+    classifier = Classifier(
+        f"{model_name}/keras_model.h5", f"{model_name}/labels.txt"
+    )
+    #chosen_sign = choose_symbol(remaining_list)
+
+    # Yield the "Please sign {chosen_sign}" message for SSE
+    #yield f"Please sign {chosen_sign}\n\n"
+    print(f"Please sign {chosen_sign}\n\n")
+
+    # Now assess the sign
+    user_img = assess_sign(model_name)
+    # cv2.destroyAllWindows()
+    prediction = get_prediction(sign_name_list, user_img, classifier)
+    old_score = score
+    score = compare_signs(chosen_sign, prediction, score, module.sign_list)
+    if old_score == score:
+        print(f"Sorry! It looks like the sign you made was {prediction}.\n\n")
+        result = f"Sorry! It looks like the sign you made was {prediction}.\n\n"
+        return [result, chosen_sign]
+        #yield f"data: Sorry! It looks like the sign you made was {prediction}.\n\n"
+    else:
+        print("Correct!\n\n")
+        result = "Correct!\n\n"
+        return [result, chosen_sign]
+        #yield f"data: Correct!\n\n"
+
+    # If desired, you can add a delay here before the next iteration
+    # (e.g., time.sleep(1) for a 1-second delay)
+
+# Yield the final score
+#yield f"data: Score: {score}/{len(module.sign_name_list)}\n\n"
 
 
+
+"""
 @app.route("/a_test")
 def a_test():
     return render_template("sse_output.html")
 
 
+
 @app.route("/sse_output")
 def sse_output():
     return Response(generate_output(MOD1), content_type="text/event-stream")
-
+"""
 
 def generate_frames():
     cap = cv2.VideoCapture(0)
@@ -271,7 +326,7 @@ def modules():
 def learn(
     module,
     sign,
-    result="Press 'Try Sign' and then hold up the sign. The capturing Process may taking a few seconds.",
+    result="Press 'Try Sign' and then hold up the sign. The capturing Process may take a few seconds.",
 ):
     global chosen_mod
     chosen_mod = search_mod_for_name(module, user_mod_data)
@@ -280,15 +335,43 @@ def learn(
     return render_template("learn.html", module=module, sign=sign, result=result)
 
 
+
+
 @app.route("/assessment/<module>/<assessmentType>")
-def assessment(module, assessmentType):
-    return render_template(
-        "assessment.html", module=module, assessmentType=assessmentType
-    )
+def assess(
+    module,
+    assessmentType,
+    result="Press 'Try Sign' and then hold up the sign. The capturing Process may take a few seconds.",
+):
+    global remaining_list
+    global chosen_mod
+    global chosen_sign
+    global is_first_sign
+    global score
+    score = 0
+    chosen_mod = search_mod_for_name(module, user_mod_data)
+    print("in assessment")
+    if assessmentType == "Basic Assessment":
+        print("basic assessment detected")
+        if len(remaining_list) == 0:
+            print("LIST EMPTY. FILLING LIST")
+            remaining_list = chosen_mod.sign_name_list.copy()
+            chosen_sign = choose_symbol(remaining_list)
+            is_first_sign = True
+        else:
+            print("List has values")
+        #global chosen_sign
+        #chosen_sign = sign
+        return render_template(
+            "basic_assessment.html", module=module, assessmentType=assessmentType, result=result, sign=chosen_sign
+        )
+    else:
+        print("PROBLEM NOT BASIC")
 
 
 @app.route("/run_sandbox_f", methods=["POST"])
 def run_sandbox_f():
+    global chosen_mod
     # Call your run_sandbox(chosen_mod) function here
     # Replace the following line with your actual logic
     result = run_sandbox(chosen_mod)  # Replace with your function call
@@ -302,6 +385,39 @@ def run_learn_sign_f(module, sign):
     res = learn_sign(chosen_mod, chosen_sign)  # Replace with your function call
     save_module_data(user_mod_data, f"{username}_data")
     return render_template("learn.html", module=module, sign=sign, result=res)
+
+@app.route("/<module>/<sign>/run_basic_assessment_f", methods=["POST"])
+def run_assessment_f(module, sign):
+
+    #global chosen_mod
+    global is_first_sign
+    global chosen_mod
+    global chosen_sign
+    # Using below to deal with edge case
+    if is_first_sign == True:
+        print("FIRST SIGN")
+        is_first_sign = False
+    else:
+        print("NOT FIRST SIGN")
+        #chosen_sign = choose_symbol(remaining_list)
+    chosen_mod = search_mod_for_name(module, user_mod_data)
+    # Call your run_sandbox(chosen_mod) function here
+    # Replace the following line with your actual logic
+    print(chosen_sign)
+    print(chosen_mod)
+    res_tuple = mar15_assessment(chosen_mod, chosen_sign)  # Replace with your function call
+    res = res_tuple[0]
+    as_sign = res_tuple[1]
+    save_module_data(user_mod_data, f"{username}_data")
+    if len(remaining_list) != 0:
+        chosen_sign = choose_symbol(remaining_list)
+    else:
+        modules = get_modules()
+        assessments = get_assessments()
+        return render_template("score.html", module=module, assessments=assessments, score=score)
+    #chosen_sign = choose_symbol(remaining_list)
+    return render_template("basic_assessment.html", module=module, sign=chosen_sign, result=res, assessmentType="Basic Assessment")
+
 
 
 @app.route("/looper")
